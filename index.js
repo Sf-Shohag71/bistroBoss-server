bbdaf2de4a94e29ca999f53874506ed4ee802110
Sf-Shohag71 <shakhfarid71@gmail.com>
2023-05-28 14:33:07 +0600

    Loaded data from database

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //Middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ti096zd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +24,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //connect to db and collect collections
+    const menuCollection = client.db("bistroBossDB").collection("menu");
+    const reviewCollection = client.db("bistroBossDB").collection("reviews");
+
+    app.get("/menu", async(req,res)=>{
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get("/reviews", async(req,res)=>{
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
